1cf825c463f884b8611f86d6b93f3016865f2c52
amaroni <amaroni@student.42.fr>
2021-11-03 12:43:59 +0100

    ajout de ft_rt_smallest_larger

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 22:39:41 by amaroni           #+#    #+#             */
-/*   Updated: 2021/10/27 13:16:59 by amaroni          ###   ########.fr       */
+/*   Updated: 2021/11/03 12:38:37 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	ft_dll_delone(t_dll **dll);
 void	ft_free_dll(t_dll **dll);
 /* instructions.c */
 void	ft_execute_instr(t_dll **a, t_dll **b, char *instr);
-void ft_execute_and_add(t_dll **dst, t_dll *new, t_dll **a, t_dll **b);
+void	ft_execute_and_add(t_dll **dst, t_dll *new, t_dll **a, t_dll **b);
 /* push.c */
 void	ft_px(t_dll **src, t_dll **dst);
 /* rotate.c */
@@ -62,13 +62,14 @@ int		ft_is_opposite_instr_adjacent(t_dll *instr);
 char	*ft_combine_instr(t_dll *instr);
 /* resolve.c */
 t_dll	*ft_resolve_for_2_and_3(t_dll *a);
-int	ft_try_instr(t_dll *a, t_dll *b, t_dll *instr);
+int		ft_try_instr(t_dll *a, t_dll *b, t_dll *instr);
 t_dll	*ft_resolve_for_4_and_5(t_dll **a);
 
 /* found.c */
 t_dll	**ft_rt_largest(t_dll *dll);
 t_dll	**ft_rt_smallest(t_dll *dll);
 t_dll	**ft_rt_element(t_dll *dll, char *value);
+t_dll	**ft_rt_smallest_larger(t_dll *dll, char *value);
 
 /* rotation_tools.c */
 void	ft_fill_rx(t_dll **instr, size_t nb, char stack_indicator);
--- src/found.c
+++ src/found.c
@@ -6,7 +6,7 @@
 /*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 17:24:43 by amaroni           #+#    #+#             */
-/*   Updated: 2021/10/27 11:26:46 by amaroni          ###   ########.fr       */
+/*   Updated: 2021/11/03 12:37:10 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,4 +70,32 @@ t_dll	**ft_rt_element(t_dll *dll, char *value)
 	if (ft_atoi((*rt)->content) == ft_atoi(value))
 		return (rt);
 	return (NULL);
+}
+
+/* Among element with values larger than input,
+ send back the address of smallest one */
+t_dll	**ft_rt_smallest_larger(t_dll *dll, char *value)
+{
+	t_dll	**rt;
+	t_dll	**tmp;
+	int		smallest_larger;
+
+	if (!dll || !value)
+		return (NULL);
+	rt = ft_rt_largest(dll);
+	smallest_larger = ft_atoi((*rt)->content);
+	tmp = &(dll->next);
+	while (*tmp != dll)
+	{
+		if (ft_atoi((*tmp)->content) > ft_atoi(value)
+			&& ft_atoi((*tmp)->content) < smallest_larger)
+		{
+			rt = tmp;
+			smallest_larger = ft_atoi((*rt)->content);
+		}
+		tmp = &((*tmp)->next);
+	}
+	if (ft_atoi((*rt)->content) < ft_atoi(value))
+		return (NULL);
+	return (rt);
 }
