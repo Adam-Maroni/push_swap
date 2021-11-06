9444bc90413170bb677a65d7e409160dcd859ee8
amaroni <amaroni@student.42.fr>
2021-11-06 05:51:33 +0100

    On corrige le bug de optimize.c mais il n'est plus a la norme. Il faut faire une refonte des fonctions d'optimisation

--- Makefile
+++ Makefile
@@ -5,13 +5,16 @@ DEBUG_FLAGS = -fsanitize=address -g
 OUT = push_swap
 SRC = $(wildcard src/*.c)
 OBJ = $(SRC:.c=.o)
+MIN = 0
+MAX = 99
+INPUT = 565 12 48 32 87 95 6
 
 
 libft_rule:
 	make -C libft/ all
 
 push_swap_rule:
-	$(CC) $(FLAGS) $(DEBUG_FLAGS) $(SRC) main.c $(LIBFT_ARCHIVE) -o $(OUT)
+	$(CC) $(FLAGS) -g $(SRC) main.c $(LIBFT_ARCHIVE) -o $(OUT)
 
 all: libft_rule push_swap_rule
 
@@ -25,3 +28,12 @@ fclean: clean
 	make -C libft/ fclean
 
 re: fclean all
+
+gdb: all
+	gdb -x ../tests/gdb_script/1.gdb --args ./$(OUT) $(INPUT)
+
+mem_check:
+	$(CC) $(FLAGS) $(DEBUG_FLAGS) $(SRC) main.c $(LIBFT_ARCHIVE) -o $(OUT)
+
+test_rule: all
+	@ARG=`ruby -e "puts ($(MIN)..$(MAX)).to_a.shuffle.join(' ')"` ; echo $$ARG ;  ./$(OUT) $$ARG | wc -l
--- src/optimize.c
+++ src/optimize.c
@@ -6,7 +6,7 @@
 /*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:36:10 by amaroni           #+#    #+#             */
-/*   Updated: 2021/10/20 17:39:29 by amaroni          ###   ########.fr       */
+/*   Updated: 2021/11/06 05:47:43 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,28 @@ int	ft_is_opposite_instr_adjacent(t_dll *instr)
 
 	one = instr;
 	two = instr->next;
-	if ((!ft_strncmp("ra", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rra", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rra", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("ra", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rb", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rrb", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rrb", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rb", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("sa", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("sa", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("sb", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("sb", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rr", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rrr", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rrr", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rr", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("ss", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("ss", two->content, ft_strlen(two->content))))
-		return (1);
+	if (two)
+	{
+		if ((!ft_strncmp("ra", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rra", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rra", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("ra", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rb", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rrb", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rrb", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rb", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("sa", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("sa", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("sb", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("sb", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rr", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rrr", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rrr", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rr", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("ss", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("ss", two->content, ft_strlen(two->content))))
+			return (1);
+	}
 	return (0);
 }
 
@@ -48,21 +51,24 @@ char	*ft_combine_instr(t_dll *instr)
 
 	one = instr;
 	two = instr->next;
-	if ((!ft_strncmp("ra", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rb", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rb", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("ra", two->content, ft_strlen(two->content))))
-		return ("rr");
-	if ((!ft_strncmp("rra", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rrb", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("rrb", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("rra", two->content, ft_strlen(two->content))))
-		return ("rrr");
-	if ((!ft_strncmp("sa", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("sb", two->content, ft_strlen(two->content)))
-		|| (!ft_strncmp("sb", one->content, ft_strlen(one->content))
-			&& !ft_strncmp("sa", two->content, ft_strlen(two->content))))
-		return ("ss");
+	if (two)
+	{
+		if ((!ft_strncmp("ra", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rb", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rb", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("ra", two->content, ft_strlen(two->content))))
+			return ("rr");
+		if ((!ft_strncmp("rra", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rrb", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("rrb", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("rra", two->content, ft_strlen(two->content))))
+			return ("rrr");
+		if ((!ft_strncmp("sa", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("sb", two->content, ft_strlen(two->content)))
+			|| (!ft_strncmp("sb", one->content, ft_strlen(one->content))
+				&& !ft_strncmp("sa", two->content, ft_strlen(two->content))))
+			return ("ss");
+	}
 	return (NULL);
 }
 
